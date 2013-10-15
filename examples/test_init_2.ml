486d54e2813634c39e7dc180d6bb43a25b5630de
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-10-15 20:56:38 +0800

    Added resp handing to retrieve actual details

--- examples/test_init_2.ml
+++ examples/test_init_2.ml
@@ -1,18 +1,31 @@
+let callback resp =
+    Firebug.console##log(Js.string "resp");
+    Firebug.console##log(resp);
+    Firebug.console##log(resp##id);
+    Firebug.console##log(resp##name);
+    Firebug.console##log(resp##email)
+
 let test_api_call () =
   let obj = Js.string "/me" in
-  let callback resp = Firebug.console##log_2("callback", resp##name) in
   let call = Js.Unsafe.fun_call
                (Js.Unsafe.variable "FB.api") [|Js.Unsafe.inject obj; Js.Unsafe.inject callback|] in
   Js._true
 
 let start_2 =
   let callback resp =
+    let _ = Firebug.console##log(resp) in
+    let scope = Js.Unsafe.obj [||] in
+    let _ = scope##scope <- Js.string "email" in
     if (resp##status == Js.string "connected") then
       test_api_call ()
     else if (resp##status == Js.string "not_authorized") then
-      Js.Unsafe.fun_call (Js.Unsafe.variable "FB.login") [||]
+      Js.Unsafe.fun_call
+        (Js.Unsafe.variable "FB.login")
+        [|Js.Unsafe.inject callback; Js.Unsafe.inject scope|]
     else
-      Js.Unsafe.fun_call (Js.Unsafe.variable "FB.login") [||]
+      Js.Unsafe.fun_call
+        (Js.Unsafe.variable "FB.login")
+        [|Js.Unsafe.inject callback; Js.Unsafe.inject scope|]
   in
   Js.Unsafe.fun_call (Js.Unsafe.variable "FB.Event.subscribe")
                      [|Js.Unsafe.inject (Js.string "auth.authResponseChange");
