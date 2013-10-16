b16d6be2666ecd68b44313e8ae6d5a0724ffc96e
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-10-16 21:07:51 +0800

    Renamed src to use new Fb library

--- examples/fb_test1.ml
+++ examples/fb_test1.ml
@@ -2,7 +2,7 @@ let app_id = "1421725008044461"
 let domain = "loris-82.comp.nus.edu.sg"
 
 let start _ =
-  let p = Init.empty_params () in
+  let p = Fb.empty_params () in
   let _ = p##appId_ <- Js.string app_id in
   let _ = p##channelUrl_ <- Js.string (domain ^ "/channel.html") in
   let _ = p##status_ <- Js._true in
@@ -10,8 +10,8 @@ let start _ =
   let _ = p##xfbml_ <- Js._true in
   let _ = Firebug.console##log_2(Js.string ("init variable constructed"),
                                  p) in
-  let init = Js.Unsafe.fun_call
-               (Js.Unsafe.variable "FB.init") [|Js.Unsafe.inject p|] in
+
+  let init = Fb.init(p) in
   let wrapped_init = fun () -> init in
   let async_init = (Js.Unsafe.coerce (Js.string "window"))##fbAsyncInit <- wrapped_init in
   let _ = Firebug.console##log_2
