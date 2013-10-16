a84d68c300773cfa201ee01895978f3f306a9e78
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-10-16 22:27:08 +0800

    Completed implementing second part via binding library

--- examples/fb_test2_p2.ml
+++ examples/fb_test2_p2.ml
@@ -7,8 +7,7 @@ let callback resp =
 
 let test_api_call () =
   let obj = Js.string "/me" in
-  let call = Js.Unsafe.fun_call
-               (Js.Unsafe.variable "FB.api") [|Js.Unsafe.inject obj; Js.Unsafe.inject callback|] in
+  let call = Fb.api obj callback in
   Js._true
 
 let start_2 =
@@ -17,14 +16,8 @@ let start_2 =
     if (resp##status == Js.string "connected") then
       test_api_call ()
     else if (resp##status == Js.string "not_authorized") then
-      Js.Unsafe.fun_call
-        (Js.Unsafe.variable "FB.login")
-        [|Js.Unsafe.inject callback|]
+      Fb.login callback
     else
-      Js.Unsafe.fun_call
-        (Js.Unsafe.variable "FB.login")
-        [|Js.Unsafe.inject callback|]
+      Fb.login callback
   in
-  Js.Unsafe.fun_call (Js.Unsafe.variable "FB.Event.subscribe")
-                     [|Js.Unsafe.inject (Js.string "auth.authResponseChange");
-                       Js.Unsafe.inject callback|]
+  Fb.subscribe callback
